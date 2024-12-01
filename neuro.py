fd505a7bc9a046f9757c75e4fbbbe8aaf328b9fa
cmolder <28611108+cmolder@users.noreply.github.com>
2024-12-01 14:37:03 -0600

    Add neuropoker module to separate logic

--- neuro.py
+++ neuro.py
@@ -1,149 +1,23 @@
 #!/usr/bin/env python3
 
+"""Run NEAT to neuro-evolve poker players using self-play.
+"""
+
 import argparse
-from dataclasses import dataclass
 import pickle
-import random
-import time
 import os
 from pathlib import Path
-from typing import Final, Optional, Tuple
+from typing import Final
 
-import neat
-import neat.parallel
-import numpy as np
-
-from simulation import extract_features
-from simulation import RandomPlayer
-from simulation import evaluate_fitness
-from simulation import NUM_PLAYERS, SMALL_BLIND_AMOUNT, BIG_BLIND_AMOUNT, STACK
+from neuropoker.model import NEATEvolution, run_neat
 
 DEFAULT_EVOLUTION_FILE: Final[Path] = Path("models/neat_poker.pkl")
+DEFAULT_CONFIG_FILE: Final[Path] = Path("config-feedforward.txt")
 DEFAULT_NUM_GENERATIONS: Final[int] = 50
 DEFAULT_NUM_CORES: Final[int] = os.cpu_count() or 1
 
 
-@dataclass
-class NEATEvolution:
-    population: neat.Population
-    stats: neat.StatisticsReporter
-    best_genome: neat.DefaultGenome
-
-
-class NEATPlayer(RandomPlayer):
-    def __init__(self, net, uuid) -> None:
-        self.net = net
-        self.uuid = uuid
-
-    def declare_action(self, valid_actions, hole_card, round_state) -> Tuple[str, int]:
-        features = extract_features(hole_card, round_state, self.uuid)
-        output = self.net.activate(features)  # Neural network output
-        chosen_action_idx = np.argmax(output)
-
-        # Following are the allowed actions
-        # 0: fold
-        # 1: call
-        # 2: raise min
-        # 3: raise 2x min
-        # 4: raise 3x min
-        # 5: raise max
-
-        if chosen_action_idx == 0:  # Fold
-            return "fold", 0
-
-        if chosen_action_idx == 1:  # Call
-            return "call", valid_actions[1]["amount"]
-
-        raise_action = valid_actions[2]
-        min_raise = raise_action["amount"]["min"]
-        max_raise = raise_action["amount"]["max"]
-        
-        if chosen_action_idx == 2:  # Raise min
-            return "raise", min_raise
-        
-        if chosen_action_idx == 3:  # Raise 2x min
-            return "raise", min_raise * 2
-
-        if chosen_action_idx == 4:  # Raise 3x min
-            return "raise", min_raise * 3
-
-        if chosen_action_idx == 5:  # Raise max
-            return "raise", max_raise # All-in
-        
-        raise ValueError(f"Invalid action index: {chosen_action_idx}")
-
-
-def evaluate_genome(genome, config, seed=None):
-    if seed is None:
-        random.seed(time.time())
-        seed = random.randint(0, 1000)
-    net = neat.nn.FeedForwardNetwork.create(genome, config)
-    player = NEATPlayer(net, "uuid-1")
-    f1 = evaluate_fitness(player, [RandomPlayer(), RandomPlayer()], seed=seed)
-    return f1
-
-
-def eval_genomes(genomes, config):
-    seed = random.randint(0, 1000)
-    for genome_id, genome in genomes:
-        genome.fitness = evaluate_genome(genome, config, seed)
-
-
-def run_neat(
-    evolution: Optional[NEATEvolution] = None, 
-    num_generations: int = 50,
-    num_cores: int = 1,
-) -> NEATEvolution:
-    """Run NEAT neuroevolution to evolve poker players.
-    
-    Parameters:
-        evolution: NEATEvolution | None
-            The initial evolution to evolve from, if any.
-        num_generations: int
-            The number of generations to run.
-        num_cores: int
-            The number of cores to use in parallel.
-
-    Returns:
-        evolution: NEATEvolution
-            The evolved population.
-    """
-    # Load configuration
-    config_path = "config-feedforward.txt"
-    config = neat.Config(
-        neat.DefaultGenome, 
-        neat.DefaultReproduction,
-        neat.DefaultSpeciesSet, 
-        neat.DefaultStagnation, 
-        config_path
-    )
-
-    # Get or create population
-    population: neat.Population = (
-        evolution.population if evolution is not None else neat.Population(config)
-    )
-
-
-    # Add reporters
-    population.add_reporter(neat.StdOutReporter(True))
-    stats = neat.StatisticsReporter()
-    population.add_reporter(stats)
-
-    # Run NEAT
-    evaluator = neat.parallel.ParallelEvaluator(num_cores, evaluate_genome)
-
-    # winner = population.run(eval_genomes, n=50)
-    # TODO: Fix type warning
-    winner: Final[neat.DefaultGenome] = population.run(  # type: ignore
-        evaluator.evaluate, 
-        n=num_generations
-    )
-
-    # print("Best genome:", winner)
-    return NEATEvolution(population, stats, winner)
-
-
-def get_args():
+def get_args() -> argparse.Namespace:
     """Get arguments for the script.
     
     Returns:
@@ -156,46 +30,72 @@ def get_args():
         "-f", "--evolution-file", 
         type=Path, 
         default=DEFAULT_EVOLUTION_FILE, 
-        help="File to read/save the evolution to/from."
+        help=(
+            "File to read/save the evolution to/from. "
+            f"(default: {DEFAULT_EVOLUTION_FILE})"
+        )
+    )
+    parser.add_argument(
+        "-m", "--config-file",
+        type=Path,
+        default=DEFAULT_CONFIG_FILE,
+        help=(
+            "File to read the NEAT model configuration from. "
+            f"(default: {DEFAULT_CONFIG_FILE})"
+        )
     )
     parser.add_argument(
         "-g", "--num-generations",
         type=int,
         default=DEFAULT_NUM_GENERATIONS,
-        help="Number of generations to run."
+        help=(
+            "Number of generations to run. "
+            f"(default: {DEFAULT_NUM_GENERATIONS})"
+        )
     )
     parser.add_argument(
         "-c", "--num-cores",
         type=int,
         default=DEFAULT_NUM_CORES,
-        help="Number of cores to use for evaluation."
+        help=(
+            "Number of cores to use for evaluation. "
+            f"(default: {DEFAULT_NUM_CORES})"
+        )
     )
 
     return parser.parse_args()
 
 
-def main():
+def main() -> None:
     """Run the script."""
 
     # Read args
     args: Final[argparse.Namespace] = get_args()
     evolution_file: Final[Path] = args.evolution_file
+    config_file: Final[Path] = args.config_file
     num_cores: Final[int] = args.num_cores
     num_generations: Final[int] = args.num_generations
 
+    print("Running with:")
+    print(f"    Evolution file : {evolution_file}")
+    print(f"    Config file    : {config_file}")
+    print(f"    Generations    : {num_generations}")
+    print(f"    Cores          : {num_cores}")
+    print()
+
     # Initialize variables
-    evolution: Optional[NEATEvolution] = None  # Evolved population
+    evolution = None  # Evolved population
 
     # Load previous evolution, if it exists
     if evolution_file.exists():
         # Load evolution
+        print(f"Loading evolution file from {evolution_file}...")
         with evolution_file.open("rb") as ef:
             evolution = pickle.load(ef)
 
             if not isinstance(evolution, NEATEvolution):
-                raise ValueError(f"Invalid evolution file: {evolution_file}")
+                raise ValueError("Evolution file is invalid")
 
-            print(f"Loaded evolution at {evolution_file}")
             print(f"Running until generation {num_generations + evolution.population.generation}...")
     else:
         # Start from scratch
@@ -203,7 +103,12 @@ def main():
         print(f"Running until generation {num_generations}...")
 
     # Run NEAT
-    evolution = run_neat(evolution, num_generations, num_cores)
+    evolution = run_neat(
+        evolution, 
+        config_file = config_file,
+        num_generations = num_generations, 
+        num_cores = num_cores
+    )
 
     # Save model
     with evolution_file.open("wb") as ef:
